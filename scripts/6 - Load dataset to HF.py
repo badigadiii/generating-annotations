04d23427779f0a2b29d6d844b0212fa7a50155d2
badigadiii <bigtinyturtle@gmail.com>
2025-05-08 05:08:26 +0400

    filtering dataset

---
+++ scripts/6 - Load dataset to HF.py
@@ -0,0 +1,14 @@
+from config_file import config
+from huggingface_hub import HfApi
+
+api = HfApi()
+
+dataset_folder = config.DATA_PATH / 'retro-games-gameplay-frames-30k-512p'
+
+# huggingface-cli upload-large-folder badigadiii/retro-games-gameplay-frames-30k-512p --repo-type dataset retro-games-gameplay-frames-30k-512p --num-workers=4
+api.upload_large_folder(
+    repo_id="badigadiii/retro-games-gameplay-frames-30k-512p",
+    repo_type="dataset",
+    folder_path=dataset_folder,
+    num_workers=4
+)
