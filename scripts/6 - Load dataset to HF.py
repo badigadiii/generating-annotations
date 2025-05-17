33d4b8e9434840e92827f3686412c718228237aa
badigadiii <bigtinyturtle@gmail.com>
2025-05-17 20:42:49 +0400

    genres analysis

--- scripts/6 - Load dataset to HF.py
+++ scripts/6 - Load dataset to HF.py
@@ -5,7 +5,7 @@ api = HfApi()
 
 dataset_folder = config.DATA_PATH / 'retro-games-gameplay-frames-30k-512p'
 
-# huggingface-cli upload-large-folder badigadiii/retro-games-gameplay-frames-30k-512p --repo-type dataset retro-games-gameplay-frames-30k-512p --num-workers=4
+# huggingface-cli upload-large-folder badigadiii/retro-games-gameplay-frames --repo-type dataset retro-games-gameplay-frames --num-workers=4
 api.upload_large_folder(
     repo_id="badigadiii/retro-games-gameplay-frames-30k-512p",
     repo_type="dataset",
